3e45d908c37c71a172bc7deffdce743144bf1283
Hayyan67 <humairabaig81@gmail.com>
2024-09-22 15:16:36 +0500

    initial  commit

---
+++ main.py
@@ -0,0 +1,9 @@
+import math
+print('Square root is: ', math.sqrt(16))
+print('2 to power 4: ',math.pow(2,4))
+print('PI: ',math.pi)
+print('Floor is: ',math.floor(3,6))
+print('Ceil is: ',math.ceil(3,6))
+print('Sin is: ',math.sin(90))
+print('Cos is: ',math.cos(90))
+print('Tan is: ',math.tan(90))
